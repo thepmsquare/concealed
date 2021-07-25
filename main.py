20f29b4ce999469b8f4e092b974581c68f03d247
Parth Mangtani <impmmmm@gmail.com>
2021-07-25 10:24:06 +0530

    add cors

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from fastapi import FastAPI
 from encode import encode as e
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*']
+)
+
 
 @app.get("/encode")
 async def encode(image, message):
-    return {"message": message}
+    return {"image": image,
+            "message": message}
 
 
 @app.get("/")
