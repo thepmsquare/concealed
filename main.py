38d4b2e47cd9c69f38d43cbbcf9a94675dd330df
Parth Mangtani <impmmmm@gmail.com>
2021-08-27 22:05:04 +0530

    test build

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    expose_headers=["Access-Control-Allow-Origin"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
