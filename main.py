f98bf14b48c2317c5d6b40abcd3362dae1c8ca08
Parth Mangtani <impmmmm@gmail.com>
2021-08-28 21:10:38 +0530

    test build

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 from fastapi import FastAPI, Form, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
 from encode import encode as e
 from decode import decode as d
 import json
 
-app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["POST", "GET"],
-    allow_headers=["*"],
-)
+middleware = [Middleware(CORSMiddleware, allow_origins=[
+                         '*'], allow_credentials=True, allow_methods=['*'], allow_headers=['*'])]
+app = FastAPI(middleware=middleware)
 
 # POST request because browsers do not allow GET request with a body
 
