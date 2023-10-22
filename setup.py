cdcd70bf858d5abb7a3c6d5fe28b46fbc861a70e
Parth Mangtani <impmmmm@gmail.com>
2023-10-22 20:22:33 +0530

    add long_description

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ setup(
     install_requires=["Pillow>=8.3.1", "cryptography>=41.0.4"],
     author="thePmSquare",
     author_email="thepmsquare@gmail.com",
-    description="a python based utility, meant to be used for encoding and decoding messages in images i.e. steganography.",
+    description="python util module to provide encryption and decryption using steganography using the least significant bits of the RGB values of an image on top of advanced encryption standard.",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/thepmsquare/concealed",
     classifiers=[
         "Development Status :: 3 - Alpha",
